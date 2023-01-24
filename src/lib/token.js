81d9bfcae008f0d3924667f522b52f4c995f8c73
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-01-24 13:05:16 -0300

    style: from 'id' to 'credentialId'
    Renaming parameter to make it easier to understand

--- src/lib/token.js
+++ src/lib/token.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 export const createAccess = (id) => {
-  const payload = { id };
+  const payload = { credentialId: id };
   const secretPass = process.env.SECRET_PASS;
   const accessToken = jwt.sign(payload, secretPass, { expiresIn: '1440m' });
   return accessToken;
