40c551f7450e31472dce0626a00f6b5b6c22beda
Samuel A. Souza <samuel.araujo.souza@outlook.com>
2023-01-26 14:51:05 -0300

    refactor(lib): url parser

--- src/lib/urlParser.js
+++ src/lib/urlParser.js
@@ -1,9 +1,26 @@
-export const urlParser = (url, ...params) => {
+// export const urlParser = (url, ...params) => {
+//   const resources = url.split('/');
+//   resources.shift();
+//   const urlParams = {};
+//   for (const [key, index] of params) {
+//     urlParams[key] = resources[index];
+//   }
+//   return urlParams
+// }
+
+export const urlParser = (url, urlMatch) => {
   const resources = url.split('/');
+  const match = urlMatch.split('/');
   resources.shift();
-  const urlParams = {};
-  for (const [key, index] of params) {
-    urlParams[key] = resources[index];
+  match.shift();
+
+  const urlParams = {}
+
+  for (const item in resources) {
+    if (resources[item] !== match[item]) {
+      const key = match[item].replace(':', '');
+      urlParams[key] = resources[item];
+    }
   }
-  return urlParams
+  return urlParams;
 }
